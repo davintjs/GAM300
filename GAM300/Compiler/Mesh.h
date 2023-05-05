4fcad3c403acc15bf63670c2d87621fbf0b41137
davintjs <davintan99@gmail.com>
2023-05-05 11:51:14 -0700

    Saving of materials and textures of fbx file

--- GAM300/Compiler/Mesh.h
+++ GAM300/Compiler/Mesh.h
@@ -2,6 +2,7 @@
 #define MESH_H
 
 #include "glm/glm.hpp"
+#include "assimp/color4.h"
 
 struct Vertex
 {
@@ -25,21 +26,27 @@ struct SampleHolder
 
 struct Material
 {
-    std::size_t GUID;
-    std::string matName;
-    std::vector<SampleHolder> _samples;
+    aiColor4D Specular; // Shiny spots on the object when light hits the surface
+    aiColor4D Diffuse; // Color when light his the surface and illuminates it
+    aiColor4D Ambient; // Color the surface has when not exposed directly to light from object
+
+    std::vector<Texture> textures;
+
+    //std::size_t GUID;
+    //std::string matName;
+    //std::vector<SampleHolder> _samples;
 };
 
 class Mesh {
 public:
-    std::vector<Vertex> _vertices;
-    std::vector<int> _indices;
+    std::vector<Vertex> _vertices; // This individual mesh vertices
+    std::vector<int> _indices; // This individual mesh indices
 
-    int _material; // sampler3d
+    int materialIndex = 0; // Material index
 public:
     Mesh() {};
     Mesh(std::vector<Vertex> vertices, std::vector<int> indices, int material)
-        :_vertices(vertices), _indices(indices), _material(material)
+        :_vertices(vertices), _indices(indices), materialIndex(material)
     {};
 
 };
