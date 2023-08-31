3f9d6f31fb1fc027016d8abc231082d52c982a77
davintjs <davintan99@gmail.com>
2023-08-31 12:21:57 +0800

    For me once again

--- GAM300/GAM300/Source/AssetManager/AssetManager.h
+++ GAM300/GAM300/Source/AssetManager/AssetManager.h
@@ -8,9 +8,7 @@
 struct Asset
 {
 	std::vector<std::filesystem::file_time_type> mAssetsTime;
-
-	std::unordered_map<int, std::pair<std::filesystem::file_time_type, std::string>> mJsonFiles;
-	std::unordered_map<int, std::pair<std::filesystem::file_time_type, std::vector<char>>> mBinaryFiles;
+	std::unordered_map<int, std::pair<std::filesystem::file_time_type, std::vector<char>>> mFilesData;
 };
 
 ENGINE_SYSTEM(AssetManager)
@@ -20,6 +18,9 @@ public:
 	void Update();
 	void Exit();
 
+	void AsyncLoadAsset(const std::string& assetPath);
+	const std::vector<char>& GetAsset(const int& assetGUID);
+
 private:
 	// Thread stuff
 	ThreadPool AssetThread;
@@ -28,4 +29,6 @@ private:
 
 	// Asset stuff
 	Asset mTotalAssets;
+
+	void LoadAsset(const std::string& filePath);
 }
