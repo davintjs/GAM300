ead04550e943d3e960f60c1ae4025230037ce932
Sean.Ngo <Prawnny@live.com>
2023-08-31 16:53:57 +0800

    Editor Templates

---
+++ GAM300/GAM300/Source/Editor/EditorTemplates.h
@@ -0,0 +1,23 @@
+/*!***************************************************************************************
+\file			EditorTemplates.h
+\project		GAM300
+\author			Joseph Ho
+\co-author      Sean Ngo
+
+\par			Course: GAM300
+\date           31/08/2023
+
+\brief
+    This file contains the declarations of the following:
+    1. Editor Templates
+
+All content © 2023 DigiPen Institute of Technology Singapore. All rights reserved.
+******************************************************************************************/
+
+#ifndef EDITORTEMPLATES_H
+#define EDITORTEMPLATES_H
+
+
+
+#endif // !EDITORTEMPLATES_H
+
