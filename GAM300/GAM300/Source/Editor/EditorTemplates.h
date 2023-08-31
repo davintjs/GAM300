afbf1c01c6a6a15d3679b8464e096b4c0cea5ff1
Sean.Ngo <Prawnny@live.com>
2023-08-31 22:29:54 +0800

    Port To PC

--- GAM300/GAM300/Source/Editor/EditorTemplates.h
+++ GAM300/GAM300/Source/Editor/EditorTemplates.h
@@ -17,6 +17,17 @@ All content © 2023 DigiPen Institute of Technology Singapore. All rights reserve
 #ifndef EDITORTEMPLATES_H
 #define EDITORTEMPLATES_H
 
+// Includes all imgui templates to be used in developing the editor, the purpose of this
+// file is to modulate the components of the editor to allow for easy-er development of the 
+// editor
+class OurGUI
+{
+    // Flags
+    template <typename T>
+    void Flags(); // Pre-initialize flags for all different editor settings
+
+
+};
 
 
 #endif // !EDITORTEMPLATES_H
