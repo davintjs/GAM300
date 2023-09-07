4f04e870cb79f162579a88f2abfd891a68df6b38
Zoocharie <fleshogre@gmail.com>
2023-09-08 02:35:37 +0800

    [Feat] GetComponent::Name<Type>() for component name

--- GAM300/GAM300/Source/Scene/Components.h
+++ GAM300/GAM300/Source/Scene/Components.h
@@ -30,8 +30,6 @@ using Vector2 = glm::vec2;
 using Vector3 = glm::vec3;
 using Vector4 = glm::vec4;
 
-#define ComponentName(Type) #Type
-
 template<typename T,typename... Ts>
 struct GetComponentTypeGroup
 {
