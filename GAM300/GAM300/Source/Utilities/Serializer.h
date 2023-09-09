cc2b4fc3aef85dc8280ce7b6906ee42df5ae8a9a
Sean.Ngo <Prawnny@live.com>
2023-09-09 16:05:59 +0800

    [Chore] Port to PC

--- GAM300/GAM300/Source/Utilities/Serializer.h
+++ GAM300/GAM300/Source/Utilities/Serializer.h
@@ -18,4 +18,44 @@ All content © 2023 DigiPen Institute of Technology Singapore. All rights reserve
 #include "YAMLUtils.h"
 #include "Properties.h"
 
+#include "glm/vec3.hpp"
+
+class Component : public property::base
+{
+public:
+    virtual ~Component() = default;
+};
+
+struct TransformComponent : public Component, public property::base
+{
+protected:
+    float position;
+    float rotation;
+    float scale;
+
+public:
+    void DefaultValues(void) noexcept
+    {
+        position = 1.f;
+        rotation = 1.f;
+        scale = 1.f;
+    }
+
+    void SanityCheck(void) const noexcept
+    {
+        assert(position == 1.f);
+        assert(rotation == 1.f);
+        assert(scale == 1.f);
+    }
+
+    property_vtable()
+};
+
+property_begin_name(TransformComponent, "Transform")
+{
+    property_var(position),
+    property_var(rotation),
+    property_var(scale)
+}
+property_vend_h(TransformComponent)
 #endif // !SERIALIZER_H
