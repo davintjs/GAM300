62452fafec6caf52382b09098a2ab1ae5400e640
Zoocharie <fleshogre@gmail.com>
2023-09-12 15:49:25 +0800

    Update GameObject.cs

--- GAM300/CoreScripts/GameObject.cs
+++ GAM300/CoreScripts/GameObject.cs
@@ -52,7 +52,7 @@ namespace BeanFactory
         //Gets a component by calling back to c++
         public T GetComponent<T>() where T : Component, new()
         {
-            return InternalCalls.GetComponent(this, typeof(T) as Component);
+            return InternalCalls.GetComponent(this, typeof(T)) as T;
         }
 
         /*        public T AddComponent<T>() where T : Component, new()
