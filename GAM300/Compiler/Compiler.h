e771749a7b42858aae6069cf7ce5394b8aa04363
davintjs <davintan99@gmail.com>
2023-09-28 22:50:14 +0800

    Comments

--- GAM300/Compiler/Compiler.h
+++ GAM300/Compiler/Compiler.h
@@ -1,3 +1,23 @@
+/*!***************************************************************************************
+\file			Compiler.h
+\project
+\author         Davin Tan
+
+\par			Course: GAM300
+\date           28/09/2023
+
+\brief
+	This file contains the declarations of the following:
+	1. Loading of FBX files with assimp loader
+		a. Creation of descriptor file for the mesh
+		b. Deserialization of FBX file
+		b. Reading and storing of meshes
+		c. Compression of vertices
+	2. Serialization of meshes into custom binary format
+
+All content © 2023 DigiPen Institute of Technology Singapore. All rights reserved.
+******************************************************************************************/
+
 #pragma once
 
 #ifndef MODEL_H
@@ -55,19 +75,35 @@ public:
 	ModelLoader(const std::string descriptorFilePath, const std::string geomFilePath);
 	~ModelLoader();
 
+	// Load the FBX file with import options to be processed
 	void LoadModel();
+
+	// Process the FBX file bones for animation
 	void ProcessBones(const aiNode& node, const aiScene& scene);
+
+	// Process the imported FBX file read from assimp library
 	void ProcessGeom(const aiNode& node, const aiScene& scene);
 	Geom_Mesh ProcessMesh(const aiMesh& mesh, const aiScene& scene);
+
+	// Optimization of vertices of the FBX model
 	void Optimize(std::vector<TempVertex>& vert, std::vector<unsigned int>& ind);
+
+	// Compression of vertices to reduce custom binary custom file size
 	void CompressVertices(std::vector<Vertex>& CompressVertices,
 							const std::vector<TempVertex> tempVertex,
 							std::pair<glm::vec3, glm::vec2>& mOffsets,
 							std::pair<glm::vec3, glm::vec2>& mScales);
+
+	// Applies the transformations stated in the descriptor file onto the FBX model
 	void TransformVertices(std::vector<TempVertex> vert);
+
+	// Import materials and textures of the FBX model
 	void ImportMaterialAndTextures(const aiMaterial& material);
 
+	// Serialization of the FBX model to custom binary format
 	void SerializeBinaryGeom(const std::string filepath);
+
+	// Deserialization of descriptor file to read the FBX model
 	void DeserializeDescriptor(const std::string filepath);
 
 private:
@@ -76,15 +112,6 @@ private:
 
 	std::vector<Geom_Mesh> _meshes{}; // Individual meshes in the model, which also contains its individual vertices and indices
 
-	//glm::vec3 mPosCompressionScale; // Scale value according to the bounding box of the vertices positions containing the whole model
-	//glm::vec2 mTexCompressionScale; // Scale value according to the bounding box of the texture coordinates containing the whole model
-
-	// I think this bottom part we should eventually phase out, and save the individual meshes
-	// vertices and indices instead of whole chunk at one go
-	//std::vector<Vertex> _vertices{}; // Total vertices of the WHOLE model
-	//std::vector<unsigned int> _indices{}; // Total indices of the WHOLE model
-
-	//std::vector<Texture> _textures{}; // Total textures of the WHOLE model
 	std::vector<Material> _materials{}; // Total materials of the WHOLE model (One mesh uses one material only)
 };
 
--- GAM300/GAM300/Source/AI/NavMeshBuilder.h
+++ GAM300/GAM300/Source/AI/NavMeshBuilder.h
@@ -1,3 +1,21 @@
+/*!***************************************************************************************
+\file			NavMeshBuilder.h
+\project
+\author         Davin Tan
+
+\par			Course: GAM300
+\date           28/09/2023
+
+\brief
+	This file contains the declarations of the following:
+	1. NavMeshBuilder singleton class
+		a. Builds the navmesh given the ground vertices and indices
+		b. Add holes and obstacles to the navmesh
+		c. Triangulation by ear clipping method
+
+All content © 2023 DigiPen Institute of Technology Singapore. All rights reserved.
+******************************************************************************************/
+
 #pragma once
 
 #include <vector>
@@ -15,29 +33,52 @@ public:
 	NavMeshBuilder() {};
 	~NavMeshBuilder() {};
 
+	// NavMeshBuilder exit
 	void Exit();
 
+	// Builds the navmesh from the given vertices and indices
 	void BuildNavMesh(const std::vector<glm::vec3>&GroundVertices, const std::vector<glm::ivec3>&GroundIndices);
 	NavMesh* CreateNavMesh();
 
+	// Getter functions
+	// Returns the region of the navmesh
 	std::vector<Polygon3D>& GetRegion();
+
+	// Returns the holes in this navmesh
 	std::vector<Polygon3D>& GetHoles();
 
 private:
+	// Compute the individual regions of the navmesh
 	std::vector<Polygon3D> ComputeRegions(const std::vector<Triangle3D>& GroundTriangles);
+
+	// Returns the ground triangles of the navmesh
 	std::vector<Triangle3D> GetGroundTriangles(const std::vector<glm::vec3>& GroundVertices, const std::vector<glm::ivec3>& GroundIndices);
-	void SetBoundary(Polygon3D* mBoundary);
+
+	// Add hole to the navmesh and editing the navmesh to accomodate the holes
 	void AddHole(Polygon3D* hole);
 	void RemoveHoles();
+
+	// Triangulation of the navmesh with ear clipping method
 	std::vector<Triangle3D> Triangulate();
+
+	// Initialize ground triangles after triangulation
 	void InitializeGroundTriangles(std::vector<Triangle3D>& GroundTriangles);
+
+	// Find neighbours with the same normal (Helper function for region creation)
 	void FindAllNeighboursWithNormal(std::vector<Triangle3D>& triPos, Triangle3D neighbourTri, const glm::vec3 triNormal);
 
 	// Helper functions
+	// Returns of the value is positive or negative
 	template <typename T>
 	int sign(const T& val);
+
+	// Determines if triangle is clockwise oriented
 	int IsTriangleOrientedClockWise(const glm::vec3& p1, const glm::vec3& p2, const glm::vec3& p3, const glm::vec3& n);
+
+	// Checks if the constructed triangle is a clockwise oriented triangle
 	bool PointsOnLine(std::vector<glm::vec3> points, const glm::vec3& n);
+
+	// Parallel check of the two vectors
 	bool Parallel(const glm::vec3& v1, const glm::vec3& v2);
 
 	int mTriCount = 0;
