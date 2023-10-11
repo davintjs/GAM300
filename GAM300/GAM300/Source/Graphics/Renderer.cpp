2911de0919712684c55ae80328bf7db26234e86f
EuanLim <lim.euan123@gmail.com>
2023-10-11 22:15:39 +0800

    little fix

--- GAM300/GAM300/Source/Graphics/Renderer.cpp
+++ GAM300/GAM300/Source/Graphics/Renderer.cpp
@@ -420,7 +420,7 @@ void Renderer::DrawMeshes(const GLuint& _vaoid, const unsigned int& _instanceCou
 			, 1, &SpotLight_Sources[i].inner_CutOff);
 
 		std::string spot_cutoff_outer;
-		spot_cutoff_outer = "spotLights[" + std::to_string(i) + "].outer_CutOff";
+		spot_cutoff_outer = "spotLights[" + std::to_string(i) + "].outerCutOff";
 		glUniform1fv(glGetUniformLocation(shader.GetHandle(), spot_cutoff_outer.c_str())
 			, 1, &SpotLight_Sources[i].outer_CutOff);
 
