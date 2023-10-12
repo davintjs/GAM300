76f0a4242396bfe9e5ac826dce3564cbd46aef9e
EuanLim <lim.euan123@gmail.com>
2023-10-12 15:42:10 +0800

    pushing for myself

--- GAM300/GAM300/Source/Graphics/Renderer.cpp
+++ GAM300/GAM300/Source/Graphics/Renderer.cpp
@@ -22,8 +22,33 @@ All content © 2023 DigiPen Institute of Technology Singapore. All rights reserve
 #include "MeshManager.h"
 #include "Editor_Camera.h"
 
+unsigned int depthMapFBO; 
+unsigned int depthMap; // Shadow Texture
+
+const unsigned int SHADOW_WIDTH = 1024, SHADOW_HEIGHT = 1024;
+
+
 void Renderer::Init()
 {
+	glGenFramebuffers(1, &depthMapFBO);
+	// create depth texture
+	glGenTextures(1, &depthMap);
+	glBindTexture(GL_TEXTURE_2D, depthMap);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT16, SHADOW_WIDTH, SHADOW_HEIGHT, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	// attach depth texture as FBO's depth buffer
+	glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depthMap, 0);
+	glDrawBuffer(GL_NONE);
+	glReadBuffer(GL_NONE);
+	if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+		std::cout << "exploded\n";
+	else
+		std::cout << "framebuffer created successfully\n";
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
 }
 
