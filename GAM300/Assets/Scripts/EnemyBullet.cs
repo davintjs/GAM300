ede67fe38a498194dc2ce3a6ba5ac0f3766fb886
Zoocharie <fleshogre@gmail.com>
2023-11-21 10:29:10 +0800

    [Debug] Bullets not spawning on enemies again...

---
+++ GAM300/Assets/Scripts/EnemyBullet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BeanFactory;
+
+public class EnemyBullet : Script
+{
+    void OnTriggerEnter(PhysicsComponent other)
+    {
+        Console.WriteLine("Enemy bullet hit something");
+        if (GetTag(other) != "Enemy")
+        {
+            Console.WriteLine("Self destructed bullet");
+            Destroy(gameObject);
+        }
+    }
+}
