1ae2f8e81d520f3ac052a485650c1e2d4f994f84
Zoocharie <fleshogre@gmail.com>
2023-11-26 19:49:19 +0800

    [Feat] Dialogue

---
+++ GAM300/Assets/Scripts/Dialogue.cs
@@ -0,0 +1,23 @@
+﻿using BeanFactory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Dialogue : Script
+{
+    public int state = 1;
+
+    public void OnTriggerEnter(PhysicsComponent component)
+    {
+        if (GetTag(component) == "Player")
+            DialogueManager.Instance.SetState(state);
+    }
+
+    public void OnTriggerExit(PhysicsComponent component)
+    {
+        if (GetTag(component) == "Player")
+            DialogueManager.Instance.Reset();
+    }
+}
