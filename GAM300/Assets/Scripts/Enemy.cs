f3a7191ea0e188f7f8c6b0b04f47658d79271234
GreenhornNew <hoonshiming99@gmail.com>
2023-12-07 16:32:02 +0800

    Added melee enemy model into the scene with basic chase behavior

--- GAM300/Assets/Scripts/Enemy.cs
+++ GAM300/Assets/Scripts/Enemy.cs
@@ -1,25 +1,45 @@
-﻿using BeanFactory;
-using System;
+﻿using System.Collections;
 using System.Collections.Generic;
-using System.Collections;
+using BeanFactory;
 using GlmSharp;
+using System;
+using System.Linq;
+using System.Security.Policy;
 
 public class Enemy : Script
 {
     public float moveSpeed = 2f;
     public float chaseDistance = 5f;
+    public float attackDistance = 3f;
     public Transform player;
     public int count;
 
-    public int Health = 1;
+    public float maxHealth = 3f;
+    public float currentHealth;
+    // HealthBar
+    public Transform hpBar;
+    public bool inRange = false;
+    public bool back = false;
 
-    //public float ching;
+    public Transform meleeEnemyPos;
+    public float RotationSpeed = 6f;
+
+
+    AnimationStateMachine animationManager;
+    public Animator animator;
+    public int state;//Example 1 is walk, 2 is attack, 3 is idle etc.
+    public bool startDeathAnimationCountdown = false;
+    float animationTimer = 3.18f;
+    public float currentAnimationTimer;
+
 
 
     void Start()
     {
-        
+        currentHealth = maxHealth;
+        InitAnimStates();
     }
+
 
     void Update()
     {
@@ -39,20 +59,78 @@ public class Enemy : Script
         }
     }
 
+    // Start is called before the first frame update
+    void InitAnimStates()
+    {
+        animationManager = new AnimationStateMachine(animator);
+        currentAnimationTimer = animationTimer;
+
+        //Highest Precedence
+        AnimationState death = animationManager.GetState("Death");
+        AnimationState stun = animationManager.GetState("Stun");
+        AnimationState falling = animationManager.GetState("Falling");
+        AnimationState jump = animationManager.GetState("Jump");
+        AnimationState attack1 = animationManager.GetState("Attack1");
+        AnimationState attack2 = animationManager.GetState("Attack2");
+        AnimationState attack3 = animationManager.GetState("Attack3");
+        AnimationState sprint = animationManager.GetState("Sprint");
+        AnimationState run = animationManager.GetState("Run");
+        //Lowest Precedence
+
+        stun.SetConditionals(false, death);
+        falling.SetConditionals(false, death, stun);
+        falling.loop = true;
+        jump.SetConditionals(false, death, stun);
+        attack1.SetConditionals(false, jump, death, stun);
+        attack2.SetConditionals(false, jump, death, stun);
+        attack3.SetConditionals(false, jump, death, stun);
+        attack1.speed = 1.5f;
+        attack2.speed = 1.5f;
+        attack3.speed = 1.5f;
+        sprint.SetConditionals(true, run);
+        sprint.SetConditionals(false, attack1, jump, death, stun);
+        sprint.loop = true;
+        run.SetConditionals(false, sprint, attack1, jump, death, stun);
+        run.loop = true;
+    }
+
+    void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+        hpBar.localScale.x = currentHealth / maxHealth;
+    }
+
     void Exit()
     {
 
     }
 
+    void OnTriggerEnter(PhysicsComponent other)
+    {
+        //check if the rigidbody belongs to a game object called PlayerWeaponCollider
+        if (GetTag(other) == "PlayerAttack")
+        {
+            Transform otherT = other.gameObject.GetComponent<Transform>();
+            vec3 dir = otherT.back;
+            dir = dir.NormalizedSafe;
+            if (damagedCoroutine != null)
+            {
+                StopCoroutine(damagedCoroutine);
+            }
+            damagedCoroutine = StartCoroutine(Damaged(.5f, dir * 5));
+            TakeDamage(1);
+        }
+    }
+
     void OnCollisionEnter(PhysicsComponent component)
     {
-        if (GetTag(component) == "PlayerCollider")
-        {
-            Console.WriteLine("I have been attacked!");
-            Health -= 1;
-            if (Health <= 0)
-                Destroy(gameObject);
-        }
+        //if (GetTag(component) == "PlayerCollider")
+        //{
+        //    Console.WriteLine("I have been attacked!");
+        //    currentHealth -= 1;
+        //    if (currentHealth <= 0)
+        //        Destroy(gameObject);
+        //}
     }
 }
 
