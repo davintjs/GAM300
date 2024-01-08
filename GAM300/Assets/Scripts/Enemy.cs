696a0d6834ccc388a9470fb7917c316ca9613d74
GreenhornNew <hoonshiming99@gmail.com>
2024-01-08 10:12:02 +0800

    Enemy behavior updated, enemy animation still not changing

--- GAM300/Assets/Scripts/Enemy.cs
+++ GAM300/Assets/Scripts/Enemy.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using BeanFactory;
 using GlmSharp;
 using System;
-using System.Linq;
-using System.Security.Policy;
+using System.Threading;
 
 public class Enemy : Script
 {
     public float moveSpeed = 2f;
     public float chaseDistance = 5f;
     public float attackDistance = 3f;
+    public float attackCooldown = 0f;
     public Transform player;
     public int count;
 
@@ -33,13 +33,14 @@ public class Enemy : Script
     public float currentAnimationTimer;
     private Coroutine damagedCoroutine = null;
 
-    public float testing = 1f;
+    public float testingValue = 0f;
 
 
 
     void Start()
     {
         currentHealth = maxHealth;
+        state = 0;//start with idle state
         InitAnimStates();
     }
 
@@ -47,18 +48,129 @@ public class Enemy : Script
     void Update()
     {
         if (player == null)
-            return;
-        //follow target
-        vec3 direction = (player.localPosition - transform.localPosition).Normalized;
-        direction.y = 0;
-        //double angle = Math.Atan2(direction.x, direction.z);
-        //vec3.Distance(player.localPosition, this.transform.localPosition);
-
-        if (vec3.Distance(player.localPosition, transform.localPosition) <= chaseDistance)
         {
-            float angle = (float)Math.Atan2(direction.x,direction.z);
-            transform.localRotation = new vec3(0,angle,0);
-            GetComponent<Rigidbody>().linearVelocity = direction * moveSpeed;
+            //SetState("Idle", true);
+            return;
+        }
+
+        ////follow target
+        //vec3 direction = (player.localPosition - transform.localPosition).Normalized;
+        //direction.y = 0;
+        ////double angle = Math.Atan2(direction.x, direction.z);
+        ////vec3.Distance(player.localPosition, this.transform.localPosition);
+
+        //if (vec3.Distance(player.localPosition, transform.localPosition) <= chaseDistance)
+        //{
+        //    SetState("Run", true);
+        //    float angle = (float)Math.Atan2(direction.x,direction.z);
+        //    transform.localRotation = new vec3(0,angle,0);
+        //    GetComponent<Rigidbody>().linearVelocity = direction * moveSpeed;
+        //}
+
+        //NOTE: testing state, remove this later
+        if (Input.GetKey(KeyCode.K))
+        {
+            Console.WriteLine("TestingState");
+            //SetState("Run", true);
+        }
+
+
+        vec3 direction = player.localPosition - transform.position;
+        direction.y = 0f;
+        direction = direction.NormalizedSafe;
+
+        switch (state)
+        {
+            //idle state
+            case 0:
+                Console.WriteLine("Idle");
+                //idle animation
+                SetState("Idle", true);
+
+                //player detection
+                if (vec3.Distance(player.localPosition, transform.localPosition) <= chaseDistance)
+                {
+                    
+                    if(animationManager.GetState("Idle").state)
+                    {
+                        SetState("Idle", false);
+                    }
+                    //change to chase state
+                    state = 1;
+                }
+                break;
+            //chase state
+            case 1:
+                Console.WriteLine("Chase");
+                SetState("Run", true);
+                //change to attack state once it has reach it is in range
+                if (vec3.Distance(player.localPosition, transform.localPosition) <= attackDistance)
+                {
+                    state = 2;
+                    attackCooldown = 0f;
+                }
+                //return to its starting position if player is far from its chaseDistance
+                if (vec3.Distance(player.localPosition, transform.localPosition) > chaseDistance)
+                {
+                    if (animationManager.GetState("Run").state)
+                    {
+                        SetState("Run", false);
+                    }
+                    //return back to its previous position state
+                    state = 0;
+                }
+                LookAt(direction);
+                GetComponent<Rigidbody>().linearVelocity = direction * moveSpeed;
+                break;
+            //attack state
+            case 2:
+                Console.WriteLine("Attack");
+                //attack animation
+                SetState("Attack", true);
+                
+                LookAt(direction);
+                //change to chase state once player has reach out of range
+                if (vec3.Distance(player.localPosition, transform.localPosition) > attackDistance)
+                {
+                    if (animationManager.GetState("Attack").state)
+                    {
+                        SetState("Attack", false);
+                    }
+                    state = 1;
+                }
+                break;
+                
+        }
+    }
+
+    void LookAt(vec3 dir)
+    {
+        if (dir == vec3.Zero)
+            return;
+        float angle = (float)Math.Atan2(dir.x, dir.z);
+        quat newQuat = glm.FromEulerToQuat(new vec3(0, angle, 0)).Normalized;
+        quat oldQuat = glm.FromEulerToQuat(transform.localRotation).Normalized;
+
+        // Interpolate using spherical linear interpolation (slerp)
+        quat midQuat = quat.SLerp(oldQuat, newQuat, Time.deltaTime * RotationSpeed);
+
+        vec3 rot = ((vec3)midQuat.EulerAngles);
+
+        if (rot != vec3.NaN)
+        {
+            bool isNan = false;
+            foreach (float val in rot)
+            {
+                if (float.IsNaN(val))
+                {
+                    isNan = true;
+                    break;
+                }
+            }
+            if (!isNan)
+            {
+                transform.localRotation = rot;
+            }
         }
     }
 
@@ -71,30 +183,29 @@ public class Enemy : Script
         //Highest Precedence
         AnimationState death = animationManager.GetState("Death");
         AnimationState stun = animationManager.GetState("Stun");
-        AnimationState falling = animationManager.GetState("Falling");
-        AnimationState jump = animationManager.GetState("Jump");
-        AnimationState attack1 = animationManager.GetState("Attack1");
-        AnimationState attack2 = animationManager.GetState("Attack2");
-        AnimationState attack3 = animationManager.GetState("Attack3");
-        AnimationState sprint = animationManager.GetState("Sprint");
+        AnimationState attack = animationManager.GetState("Attack");
+        AnimationState walk = animationManager.GetState("Walk");
         AnimationState run = animationManager.GetState("Run");
+        AnimationState idle = animationManager.GetState("Idle");
         //Lowest Precedence
 
         stun.SetConditionals(false, death);
-        falling.SetConditionals(false, death, stun);
-        falling.loop = true;
-        jump.SetConditionals(false, death, stun);
-        attack1.SetConditionals(false, jump, death, stun);
-        attack2.SetConditionals(false, jump, death, stun);
-        attack3.SetConditionals(false, jump, death, stun);
-        attack1.speed = 1.5f;
-        attack2.speed = 1.5f;
-        attack3.speed = 1.5f;
-        sprint.SetConditionals(true, run);
-        sprint.SetConditionals(false, attack1, jump, death, stun);
-        sprint.loop = true;
-        run.SetConditionals(false, sprint, attack1, jump, death, stun);
+        attack.SetConditionals(false, death, stun);
+        attack.speed = 1.5f;
+        walk.SetConditionals(true, walk);
+        walk.SetConditionals(false, attack, death, stun);
+        walk.loop = true;
+        run.SetConditionals(false, attack, death, stun);
         run.loop = true;
+    }
+
+    bool GetState(string stateName)
+    {
+        return animationManager.GetState(stateName).state;
+    }
+    void SetState(string stateName, bool value)
+    {
+        animationManager.GetState(stateName).state = value;
     }
 
     void TakeDamage(int amount)
