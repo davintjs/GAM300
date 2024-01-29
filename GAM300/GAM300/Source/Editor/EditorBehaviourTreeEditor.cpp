19562bccdfa2255b391972fc5c1baf3ef51a90cd
JosephHoJJ <2100909@sit.singaporetech.edu.sg>
2024-01-29 17:23:30 +0800

    [WIP] Behavior Tree Editor base

--- GAM300/GAM300/Source/Editor/EditorBehaviourTreeEditor.cpp
+++ GAM300/GAM300/Source/Editor/EditorBehaviourTreeEditor.cpp
@@ -15,199 +15,126 @@ All content © 2023 DigiPen Institute of Technology Singapore. All rights reserve
 #include "Precompiled.h"
 #include "EditorHeaders.h"
 #include "Core/EventsManager.h"
-#include <imgui_node_editor.h>
 
 namespace ed = ax::NodeEditor;
 
-struct LinkInfo
-{
-    ed::LinkId Id;
-    ed::PinId  InputId;
-    ed::PinId  OutputId;
-};
-
-//Control Flow, Decorator, Leaf Nodes
-//inherit from parent
-
-//Data inside nodes will be node filename (string)
-//Node rules
-
-ed::EditorContext* m_Context = nullptr;    // Editor context, required to trace a editor state.
-bool                 m_FirstFrame = true;    // Flag set for first frame only, some action need to be executed once.
-ImVector<LinkInfo>   m_Links;                // List of live links. It is dynamic unless you want to create read-only view over nodes.
-int                  m_NextLinkId = 100;     // Counter to help generate link ids. In real application this will probably based on pointer to user data structure.
-
-
-void ImGuiEx_BeginColumn()
-{
+void ImGuiEx_BeginColumn() {
     ImGui::BeginGroup();
 }
 
-void ImGuiEx_NextColumn()
-{
+void ImGuiEx_NextColumn() {
     ImGui::EndGroup();
     ImGui::SameLine();
     ImGui::BeginGroup();
 }
 
-void ImGuiEx_EndColumn()
-{
+void ImGuiEx_EndColumn() {
     ImGui::EndGroup();
 }
 
-void EditorBehaviourTreeEditor::Init()
-{
+void EditorBehaviourTreeEditor::Init() {
     m_Context = ed::CreateEditor();
+    m_Nodes.push_back(CreateNode(uniqueId, NodeType::ControlFlow, "ControlNode1.bt", ImVec2(10, 10)));
+    m_Nodes.push_back(CreateNode(uniqueId, NodeType::Decorator, "DecoratorNode1.bt", ImVec2(210, 60)));
+    m_Nodes.push_back(CreateNode(uniqueId, NodeType::Leaf, "BehaviorTree1.bt", ImVec2(100, 150)));
+    m_Nodes.push_back(CreateNode(uniqueId, NodeType::Leaf, "BehaviorTree2.bt", ImVec2(300, 150)));
 }
 
-void EditorBehaviourTreeEditor::Update(float dt)
-{
-   
+void EditorBehaviourTreeEditor::Update(float dt) {
     UNREFERENCED_PARAMETER(dt);
     ed::SetCurrentEditor(m_Context);
+    ed::Begin("Behaviour Tree Editor");
 
-    // Start interaction with editor.
-    ed::Begin("My Editor", ImVec2(0.0, 0.0f));
+    // Draw nodes
+    for (auto& node : m_Nodes) {
+        DrawNode(node);
+    }
 
-    int uniqueId = 1;
-
-    //
-    // 1) Commit known data to editor
-    //
-
-    // Submit Node A
-    ed::NodeId nodeA_Id = uniqueId++;
-    ed::PinId  nodeA_InputPinId = uniqueId++;
-    ed::PinId  nodeA_OutputPinId = uniqueId++;
-
-    if (m_FirstFrame)
-        ed::SetNodePosition(nodeA_Id, ImVec2(10, 10));
-    ed::BeginNode(nodeA_Id);
-    ImGui::Text("Node A");
-    ed::BeginPin(nodeA_InputPinId, ed::PinKind::Input);
-    ImGui::Text("-> In");
-    ed::EndPin();
-    ImGui::SameLine();
-    ed::BeginPin(nodeA_OutputPinId, ed::PinKind::Output);
-    ImGui::Text("Out ->");
-    ed::EndPin();
-    ed::EndNode();
-
-    // Submit Node B
-    ed::NodeId nodeB_Id = uniqueId++;
-    ed::PinId  nodeB_InputPinId1 = uniqueId++;
-    ed::PinId  nodeB_InputPinId2 = uniqueId++;
-    ed::PinId  nodeB_OutputPinId = uniqueId++;
-
-    if (m_FirstFrame)
-        ed::SetNodePosition(nodeB_Id, ImVec2(210, 60));
-    ed::BeginNode(nodeB_Id);
-    ImGui::Text("Node B");
-    ImGuiEx_BeginColumn();
-    ed::BeginPin(nodeB_InputPinId1, ed::PinKind::Input);
-    ImGui::Text("-> In1");
-    ed::EndPin();
-    ed::BeginPin(nodeB_InputPinId2, ed::PinKind::Input);
-    ImGui::Text("-> In2");
-    ed::EndPin();
-    ImGuiEx_NextColumn();
-    ed::BeginPin(nodeB_OutputPinId, ed::PinKind::Output);
-    ImGui::Text("Out ->");
-    ed::EndPin();
-    ImGuiEx_EndColumn();
-    ed::EndNode();
-
-    // Submit Links
-    for (auto& linkInfo : m_Links)
+    // Draw links
+    for (auto& linkInfo : m_Links) {
         ed::Link(linkInfo.Id, linkInfo.InputId, linkInfo.OutputId);
+    }
 
-    //
-    // 2) Handle interactions
-    //
-
-    // Handle creation action, returns true if editor want to create new object (node or link)
-    if (ed::BeginCreate())
-    {
+    // Handle interactions (creation, deletion)
+    if (ed::BeginCreate()) {
         ed::PinId inputPinId, outputPinId;
-        if (ed::QueryNewLink(&inputPinId, &outputPinId))
-        {
-            // QueryNewLink returns true if editor want to create new link between pins.
-            //
-            // Link can be created only for two valid pins, it is up to you to
-            // validate if connection make sense. Editor is happy to make any.
-            //
-            // Link always goes from input to output. User may choose to drag
-            // link from output pin or input pin. This determine which pin ids
-            // are valid and which are not:
-            //   * input valid, output invalid - user started to drag new ling from input pin
-            //   * input invalid, output valid - user started to drag new ling from output pin
-            //   * input valid, output valid   - user dragged link over other pin, can be validated
-
-            if (inputPinId && outputPinId) // both are valid, let's accept link
-            {
-                // ed::AcceptNewItem() return true when user release mouse button.
-                if (ed::AcceptNewItem())
-                {
-                    // Since we accepted new link, lets add one to our list of links.
+        if (ed::QueryNewLink(&inputPinId, &outputPinId)) {
+            if (inputPinId && outputPinId) {
+                if (ed::AcceptNewItem()) {
                     m_Links.push_back({ ed::LinkId(m_NextLinkId++), inputPinId, outputPinId });
-
-                    // Draw new link.
                     ed::Link(m_Links.back().Id, m_Links.back().InputId, m_Links.back().OutputId);
                 }
-
-                // You may choose to reject connection between these nodes
-                // by calling ed::RejectNewItem(). This will allow editor to give
-                // visual feedback by changing link thickness and color.
             }
         }
     }
-    ed::EndCreate(); // Wraps up object creation action handling.
+    ed::EndCreate();
 
-
-    // Handle deletion action
-    if (ed::BeginDelete())
-    {
-        // There may be many links marked for deletion, let's loop over them.
+    if (ed::BeginDelete()) {
         ed::LinkId deletedLinkId;
-        while (ed::QueryDeletedLink(&deletedLinkId))
-        {
-            // If you agree that link can be deleted, accept deletion.
-            if (ed::AcceptDeletedItem())
-            {
-                // Then remove link from your data.
-                for (auto& link : m_Links)
-                {
-                    if (link.Id == deletedLinkId)
-                    {
-                        m_Links.erase(&link);
-                        break;
-                    }
-                }
+        while (ed::QueryDeletedLink(&deletedLinkId)) {
+            if (ed::AcceptDeletedItem()) {
+                m_Links.erase(std::remove_if(m_Links.begin(), m_Links.end(), [deletedLinkId](LinkInfo& link) {
+                    return link.Id == deletedLinkId;
+                    }), m_Links.end());
             }
-
-            // You may reject link deletion by calling:
-            // ed::RejectDeletedItem();
         }
     }
-    ed::EndDelete(); // Wrap up deletion action
+    ed::EndDelete();
 
-    // End of interaction with editor.
     ed::End();
 
     if (m_FirstFrame)
         ed::NavigateToContent(0.0f);
 
     ed::SetCurrentEditor(nullptr);
-
     m_FirstFrame = false;
-
-    // ImGui::ShowMetricsWindow();
-
 }
 
-
-void EditorBehaviourTreeEditor::Exit()
-{
+void EditorBehaviourTreeEditor::Exit() {
     ed::DestroyEditor(m_Context);
-    //ed::EditorContext* m_Context = nullptr;
+}
+
+EditorBehaviourTreeEditor::Node EditorBehaviourTreeEditor::CreateNode(int& id, NodeType type, const std::string& filename, const ImVec2& position) {
+    switch (type) {
+    case NodeType::ControlFlow:
+        return ControlFlowNode(id, filename, position);
+    case NodeType::Decorator:
+        return DecoratorNode(id, filename, position);
+    case NodeType::Leaf:
+        return LeafNode(id, filename, position);
+    }
+}
+
+void EditorBehaviourTreeEditor::DrawNode(Node& node) {
+    ed::BeginNode(node.id);
+
+    ImGui::Columns(1, "header", false);
+    ImGui::NextColumn();
+    ImGui::Text(node.filename.c_str());
+
+    switch (node.type) {
+        case NodeType::ControlFlow:
+            ImGui::Text("Control Flow Node");
+            break;
+        case NodeType::Decorator:
+            ImGui::Text("Decorator Node");
+            break;
+        case NodeType::Leaf: {
+            ImGui::Text("Leaf Node");
+            break;
+        }
+    }
+
+    ed::BeginPin(node.inputPinId, ed::PinKind::Input);
+    ImGui::Text("<- Input");
+    ed::EndPin();
+
+    ImGui::Dummy(ImVec2(20, 0));    
+    ed::BeginPin(node.outputPinId, ed::PinKind::Output);
+    ImGui::Text("Output ->");
+    ed::EndPin();
+
+
+    // Add drawing logic for pins, etc.
+    ed::EndNode();
 }
