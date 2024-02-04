cd8f22c5a382ca16cc6e9fe0baa753cd7dc4bd3a
Zoocharie <fleshogre@gmail.com>
2024-02-04 20:02:17 +0800

    [Fix] Propellor spinning in different angles

--- GAM300/Assets/Scripts/SpinningPropeller.cs
+++ GAM300/Assets/Scripts/SpinningPropeller.cs
@@ -10,15 +10,9 @@ public class SpinningPropeller : Script
 {
     public float rotateSpeed = 3f;
     public bool rotateClockwise = true;
-    public bool rotateAntiClockwise = false;
-
-    public bool originalRotation = true;
-
-    vec3 tempTransform;
 
     void Start()
     {
-        tempTransform = transform.up;
     }
 
     void Update()
@@ -29,50 +23,12 @@ public class SpinningPropeller : Script
 
     void rotation()
     {
-        if (originalRotation == true)
-        {
-            //ORIGINAL ROTATION
-            if (rotateClockwise && !rotateAntiClockwise)
-            {
-                rotateAntiClockwise = false;
-                //GetComponent<Rigidbody>().angularVelocity.z = gameObject.transform.position.z * -rotateSpeed;
-                vec3 rot = transform.rotation + new vec3(0f, 0f, 10f) * Time.deltaTime * rotateSpeed;
-
-                //reset rotation so it only rotates z axis
-
-                transform.rotation = rot;
-            }
-            if (rotateAntiClockwise && !rotateClockwise)
-            {
-                rotateClockwise = false;
-                //GetComponent<Rigidbody>().angularVelocity = new vec3(gameObject.transform.position) * rotateSpeed;
-                vec3 rot = transform.rotation + new vec3(0f, 0f, -10f) * Time.deltaTime * rotateSpeed;
-
-                transform.rotation = rot;
-            }
-        }
-
-        if (originalRotation == false)
-        {
-            //ORIGINAL ROTATION
-            if (rotateClockwise && !rotateAntiClockwise)
-            {
-                rotateAntiClockwise = false;
-                //GetComponent<Rigidbody>().angularVelocity.z = gameObject.transform.position.z * -rotateSpeed;
-                vec3 rot = transform.rotation + tempTransform * Time.deltaTime * rotateSpeed;
-
-                //reset rotation so it only rotates z axis
-
-                transform.rotation = rot;
-            }
-            if (rotateAntiClockwise && !rotateClockwise)
-            {
-                rotateClockwise = false;
-                //GetComponent<Rigidbody>().angularVelocity = new vec3(gameObject.transform.position) * rotateSpeed;
-                vec3 rot = transform.rotation + tempTransform * Time.deltaTime * -rotateSpeed;
-
-                transform.rotation = rot;
-            }
-        }
+        vec3 rot = transform.localRotation;
+        //Rotation of the propellor
+        if (rotateClockwise)
+            rot += new vec3(0f, 0f, 10f) * Time.deltaTime * rotateSpeed;
+        else
+            rot += new vec3(0f, 0f, -10f) * Time.deltaTime * rotateSpeed;
+        transform.localRotation = rot;
     }
 }
