5c1ba57fd8f521fbcc0b60972f54a894d4062341
Zoocharie <fleshogre@gmail.com>
2024-02-04 21:17:25 +0800

    [WIP] Loading

---
+++ GAM300/Assets/Scripts/LoadingIcon.cs
@@ -0,0 +1,26 @@
+﻿using BeanFactory;
+using GlmSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class LoadingIcon : Script
+{
+    SpriteRenderer spriteRenderer;
+
+    public float speed = 1f;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update ()
+    {
+        if (spriteRenderer == null)
+            return;
+        transform.localRotation += new vec3(0, 0, 1) * speed * Time.deltaTime;
+    }
+}
