3a76a5dac88bcf7eb4ed17050dff7278d74565ac
JosephHoJJ <89516454+JosephHoJJ@users.noreply.github.com>
2024-02-09 04:05:03 +0800

    Added objective to levelplay2 scene

---
+++ GAM300/Assets/Scripts/ObjectiveManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using BeanFactory;
+using GlmSharp;
+
+public class ObjectiveManager : Script
+{
+    public float alphaValue = 0.8f;
+    public Transform player;
+    public SpriteRenderer Objective1;
+
+
+    bool temp = false;
+
+    //vec3 startGridTextSize;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        if(player.position.z < -43f)
+        {
+            if (!temp)
+            {
+                temp = true;
+                Objective1.alpha = alphaValue;
+                AudioManager.instance.itemCollected.Play();
+            }
+            
+        }
+            
+    }
+
+   
+
+
+}
