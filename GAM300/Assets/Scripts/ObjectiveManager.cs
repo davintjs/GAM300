d11493a714322b06867fc77c015817cc63ad6364
JosephHoJJ <89516454+JosephHoJJ@users.noreply.github.com>
2024-02-29 03:46:16 +0800

    [WIP] Dialogue and triggers

--- GAM300/Assets/Scripts/ObjectiveManager.cs
+++ GAM300/Assets/Scripts/ObjectiveManager.cs
@@ -14,17 +14,35 @@ public class ObjectiveManager : Script
     public SpriteRenderer Objective1;
 
 
+    private bool d1;
+    public SpriteRenderer Dialogue1;
+    public AudioSource audio1;
+
+
     bool temp = false;
 
     //vec3 startGridTextSize;
 
     void Start()
     {
+        //timer = 17f;
+        d1 = false;
     }
 
     void Update()
     {
-        if(player.position.z < -43f)
+        //if (d1 && (timer > 0.f))
+        //{
+        //    timer -= Time.deltaTime;
+        //}
+        //else
+        //    Objective1.alpha = 0.f;
+    }
+
+    void OnTriggerEnter(PhysicsComponent rb)
+    {
+        //detect the player
+        if (GetTag(rb) == "StartObjective")
         {
             if (!temp)
             {
@@ -32,12 +50,18 @@ public class ObjectiveManager : Script
                 Objective1.alpha = alphaValue;
                 AudioManager.instance.itemCollected.Play();
             }
-            
         }
-            
+
+        if (GetTag(rb) == "Dialogue1")
+        {
+            if (!d1)
+            {
+                d1 = true;
+                Dialogue1.alpha = 1f;
+                audio1.Play();
+            }
+        }
+
     }
-
-   
-
 
 }
