60e1f324a908261b96190c1a68855839ef946b0c
khaikiat <90824928+khaikiat@users.noreply.github.com>
2024-03-02 19:50:47 +0800

    working atlas, now do proper UV stuff

--- GAM300/GAM300/Source/Graphics/Text.cpp
+++ GAM300/GAM300/Source/Graphics/Text.cpp
@@ -39,113 +39,185 @@ void TextSystem::Exit()
 }
 
 void GenerateTextureAtlas(TextSystem::FontCharacters& characters) {
-	const int atlasSize = 1024; // Adjust this as needed
+	//const int atlasSize = 128*8152; // Adjust this as needed
 
-	// Create a blank texture atlas
-	unsigned int textureAtlas;
-	glGenTextures(1, &textureAtlas);
-	glBindTexture(GL_TEXTURE_2D, textureAtlas);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, atlasSize, atlasSize, 0, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
+	//// Create a blank texture atlas
+	//unsigned int textureAtlas;
+	//glGenTextures(1, &textureAtlas);
+	//glBindTexture(GL_TEXTURE_2D, textureAtlas);
+	//glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, atlasSize, atlasSize, 0, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
 
-	int xOffset = 0;
-	int yOffset = 0;
+	//int xOffset = 0;
+	//int yOffset = 0;
 
-	// Populate the texture atlas with individual character textures
-	for (const auto& pair : characters) {
-		const TextSystem::Character& character = pair.second;
+	//// Populate the texture atlas with individual character textures
+	//for (auto& pair : characters) {
+	//	TextSystem::Character& character = pair.second;
 
-		// Copy the character's texture to the atlas
-		glTexSubImage2D(GL_TEXTURE_2D, 0, xOffset, yOffset, character.Size.x, character.Size.y, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
+	//	// Copy the character's texture to the atlas
 
-		// Update the character's texture coordinates
-		characters[pair.first].Texture = textureAtlas;
-		characters[pair.first].Bearing.x = xOffset;
-		characters[pair.first].Bearing.y = yOffset;
+	//	//characters[pair.first];
 
-		// Move the offset for the next character
-		xOffset += character.Size.x;
+	//	// Update the character's texture coordinates
+	//	
+	//	character.Texture = textureAtlas;
+	//	character.Bearing.x = xOffset;
+	//	character.Bearing.y = yOffset;
 
-		// Check if we need to move to the next row
-		if (xOffset + character.Size.x > atlasSize) {
-			xOffset = 0;
-			yOffset += character.Size.y;
-		}
+	//	// Move the offset for the next character
+	//	xOffset += character.Size.x;
+	//	glTexSubImage2D(GL_TEXTURE_2D, 0, xOffset, yOffset, character.Size.x, character.Size.y, GL_RGBA, GL_UNSIGNED_BYTE, character.TextureData.data());
+	//	//glTexSubImage2D(GL_TEXTURE_2D, 0, xOffset, yOffset, character.Size.x, character.Size.y, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
+	//	std::cout << pair.first << " - it is working\n";
+	//	// Check if we need to move to the next row
+	//	if (xOffset + character.Size.x > atlasSize) {
+	//		xOffset = 0;
+	//		yOffset += character.Size.y;
+	//	}
+
+	//}
+
+	//// Set texture parameters
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	//glBindTexture(GL_TEXTURE_2D, 0);
+
+	// Define some constants for padding between characters
+	const int padding = 1;
+
+	// Calculate total size required for the atlas
+	int totalWidth = 0;
+	int maxHeight = 0;
+	for (auto& pair : characters) {
+		TextSystem::Character& ch = pair.second;
+		totalWidth += ch.Size.x + padding;
+		maxHeight = std::max(maxHeight, ch.Size.y);
 	}
 
+	// Create the texture atlas
+	GLuint atlasTextureID;
+	glGenTextures(1, &atlasTextureID);
+	glBindTexture(GL_TEXTURE_2D, atlasTextureID);
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1); // Set alignment to 1 byte
+
+	// Allocate memory for the texture atlas
+	std::vector<unsigned char> atlasData(totalWidth * maxHeight * 4, 0); // 4 components (RGBA)
+
 	// Set texture parameters
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, totalWidth, maxHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, atlasData.data());
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-	glBindTexture(GL_TEXTURE_2D, 0);
-}
-
-
-void TextSystem::RenderText(GLSLShader& s, std::string text, float x, float y, float scale, glm::vec3 color, BaseCamera& _camera, const Engine::GUID<FontAsset>& _guid)
-{
-	scale *= 0.001f; // hack bc it too big
-
-	glEnable(GL_BLEND);
-	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-	// activate corresponding render state	
-	s.Use();
-	//s.SetUniform(,)
-	glUniform3f(glGetUniformLocation(s.GetHandle(), "textColor"), color.x, color.y, color.z);
-	glActiveTexture(GL_TEXTURE0);
-	glBindVertexArray(txtVAO);
-
-	glm::mat4 OrthoProjection = glm::ortho(-1.f, 1.f, -1.f, 1.f, -10.f, 10.f);
-	//s.SetUniform("projection", proj);
-	glUniformMatrix4fv(glGetUniformLocation(s.GetHandle(), "projection"),
-		1, GL_FALSE, glm::value_ptr(OrthoProjection));
-
-	for (const char& c : text) {
-		Character ch = mFontContainer.at(_guid).at(c);  // Use .at() to ensure the character exists in the map
-
-		float xpos = x + ch.Bearing.x * scale;
-		float ypos = y - (ch.Size.y - ch.Bearing.y) * scale;
-
-		float w = ch.Size.x * scale;
-		float h = ch.Size.y * scale * 2.f;
-		
-		// Generate OpenGL texture from the loaded texture data ->> can optimise so it doesnt get texture every frame
-		GLuint texture;
-		glGenTextures(1, &texture);
-		glBindTexture(GL_TEXTURE_2D, texture);
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, ch.Size.x, ch.Size.y, 0, GL_RED, GL_UNSIGNED_BYTE, ch.TextureData.data());
-		glGenerateMipmap(GL_TEXTURE_2D);
-
-
-		// Update content of VBO memory with the new vertices
-		float vertices[4][4] = {
-			{ xpos,     ypos,       0.0f, 1.0f },
-			{ xpos,     ypos + h,   0.0f, 0.0f },
-			{ xpos + w, ypos,       1.0f, 1.0f },
-			{ xpos + w, ypos + h,   1.0f, 0.0f }
-		};
-
-		glBindBuffer(GL_ARRAY_BUFFER, txtVBO);
-		glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices);
-		glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-		// Use the new texture for rendering
-		glBindTexture(GL_TEXTURE_2D, texture);
-
-		// Render quad
-		glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
-
-		// Delete the generated texture when it's no longer needed
-		glDeleteTextures(1, &texture);
-
-		// now advance cursors for next glyph (note that advance is number of 1/64 pixels)
-		x += (ch.Advance >> 6) * scale; // bitshift by 6 to get value in pixels (2^6 = 64)
+	// Fill the atlas with characters
+	int xOffset = 0;
+	for (auto& pair : characters) {
+		TextSystem::Character& ch = pair.second;
+		// Copy character texture data to the atlas
+		for (int y = 0; y < ch.Size.y; ++y) {
+			for (int x = 0; x < ch.Size.x; ++x) {
+				int atlasIndex = ((y * totalWidth + xOffset + x));
+				int textureIndex = (y * ch.Size.x + x);
+				atlasData[atlasIndex] = ch.TextureData[textureIndex];
+				/*atlasData[atlasIndex + 0] = ch.TextureData[textureIndex + 0];
+				atlasData[atlasIndex + 1] = ch.TextureData[textureIndex + 1];
+				atlasData[atlasIndex + 2] = ch.TextureData[textureIndex + 2];
+				atlasData[atlasIndex + 3] = ch.TextureData[textureIndex + 3];*/
+			}
+		}
+		// Update character bearing
+		ch.Bearing.x = xOffset;
+		ch.Bearing.y = ch.Size.y - ch.Bearing.y; // Adjust bearing based on top-left origin
+		// Move xOffset to the next character
+		xOffset += ch.Size.x + padding;
 
 	}
-	glBindVertexArray(0);
+
+
+	// Update the texture atlas with the new data
+	glBindTexture(GL_TEXTURE_2D, atlasTextureID);
+	glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, totalWidth, maxHeight, GL_RGBA, GL_UNSIGNED_BYTE, atlasData.data());
+
+	// Update character advance for rendering
+	for (auto& pair : characters) {
+		TextSystem::Character& ch = pair.second;
+		ch.Advance = ch.Size.x + padding;
+	}
+
+	// Cleanup
 	glBindTexture(GL_TEXTURE_2D, 0);
-	s.UnUse();
+
 }
+
+
+//void TextSystem::RenderText(GLSLShader& s, std::string text, float x, float y, float scale, glm::vec3 color, BaseCamera& _camera, const Engine::GUID<FontAsset>& _guid)
+//{
+//	scale *= 0.001f; // hack bc it too big
+//
+//	glEnable(GL_BLEND);
+//	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+//	// activate corresponding render state	
+//	s.Use();
+//	//s.SetUniform(,)
+//	glUniform3f(glGetUniformLocation(s.GetHandle(), "textColor"), color.x, color.y, color.z);
+//	glActiveTexture(GL_TEXTURE0);
+//	glBindVertexArray(txtVAO);
+//
+//	glm::mat4 OrthoProjection = glm::ortho(-1.f, 1.f, -1.f, 1.f, -10.f, 10.f);
+//	//s.SetUniform("projection", proj);
+//	glUniformMatrix4fv(glGetUniformLocation(s.GetHandle(), "projection"),
+//		1, GL_FALSE, glm::value_ptr(OrthoProjection));
+//
+//	for (const char& c : text) {
+//		Character ch = mFontContainer.at(_guid).at(c);  // Use .at() to ensure the character exists in the map
+//
+//		float xpos = x + ch.Bearing.x * scale;
+//		float ypos = y - (ch.Size.y - ch.Bearing.y) * scale;
+//
+//		float w = ch.Size.x * scale;
+//		float h = ch.Size.y * scale * 2.f;
+//		
+//		// Generate OpenGL texture from the loaded texture data ->> can optimise so it doesnt get texture every frame
+//		GLuint texture;
+//		glGenTextures(1, &texture);
+//		glBindTexture(GL_TEXTURE_2D, texture);
+//		glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, ch.Size.x, ch.Size.y, 0, GL_RED, GL_UNSIGNED_BYTE, ch.TextureData.data());
+//		glGenerateMipmap(GL_TEXTURE_2D);
+//
+//
+//		// Update content of VBO memory with the new vertices
+//		float vertices[4][4] = {
+//			{ xpos,     ypos,       0.0f, 1.0f },
+//			{ xpos,     ypos + h,   0.0f, 0.0f },
+//			{ xpos + w, ypos,       1.0f, 1.0f },
+//			{ xpos + w, ypos + h,   1.0f, 0.0f }
+//		};
+//
+//		glBindBuffer(GL_ARRAY_BUFFER, txtVBO);
+//		glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices);
+//		glBindBuffer(GL_ARRAY_BUFFER, 0);
+//
+//		// Use the new texture for rendering
+//		glBindTexture(GL_TEXTURE_2D, texture);
+//
+//		// Render quad
+//		glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+//
+//		// Delete the generated texture when it's no longer needed
+//		glDeleteTextures(1, &texture);
+//
+//		// now advance cursors for next glyph (note that advance is number of 1/64 pixels)
+//		x += (ch.Advance >> 6) * scale; // bitshift by 6 to get value in pixels (2^6 = 64)
+//
+//	}
+//	glBindVertexArray(0);
+//	glBindTexture(GL_TEXTURE_2D, 0);
+//	s.UnUse();
+//}
 
 void TextSystem::Draw(BaseCamera& _camera)
 {
@@ -365,7 +437,7 @@ void TextSystem::AddFont(const std::filesystem::path& inputPath, const Engine::G
 	}
 	//Characters.insert(std::pair<std::string, std::map<char, Character>>(c, haracter));
 
-	//GenerateTextureAtlas(Characters);
+	GenerateTextureAtlas(Characters);
 	mFontContainer.insert(std::make_pair(_guid, Characters));
 
 	inFile.close();
