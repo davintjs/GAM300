8877a62da6acdb1dd339f097cd1e2195665fb1da
kurasan54 <41406014+kurasan54@users.noreply.github.com>
2024-03-14 21:05:27 +0800

    Overdrive Chip
    Overdrive chip is to enable the player in using overdrive.

---
+++ GAM300/Assets/Scripts/OverdriveKey.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BeanFactory;
+using GlmSharp;
+
+public class OverdriveKey : Script
+{
+    public bool moving = false;
+    public bool temp = false;
+
+    public bool back = false;
+    public float floatKey = 1f;
+    float keyStartY;
+    public float maxTime = 5;
+    float timer = 0;
+
+    void Start()
+    {
+        keyStartY = transform.localPosition.y;
+    }
+
+
+    void Update()
+    {
+
+        vec3 positive = new vec3(transform.localPosition);
+        positive.y = floatKey + keyStartY;
+        vec3 negative = new vec3(transform.localPosition);
+        negative.y = -floatKey + keyStartY;
+        if (!back)
+        {
+            transform.localPosition = vec3.Lerp(negative, positive, timer / maxTime);
+        }
+        else
+        {
+            transform.localPosition = vec3.Lerp(positive, negative, timer / maxTime);
+        }
+
+        timer += Time.deltaTime;
+        if (timer >= maxTime)
+        {
+            back = !back;
+            timer = 0;
+        }
+
+    }
+    void OnTriggerEnter(PhysicsComponent rb)
+    {
+        //detect the player
+        if (GetTag(rb) == "Player")
+        {
+            ThirdPersonController.instance.overDriveBar.SetActive(true);
+            ThirdPersonController.instance.isOverdriveEnabled = true;
+            AudioManager.instance.keyCollected.Play();
+            gameObject.SetActive(false);
+        }
+    }
+}
